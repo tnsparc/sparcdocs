d681d2bf549512487784d7b4b0c31322274bbbf9
John Crouch <john.crouch@me.com>
2025-05-15 19:48:17 -0500

    update domain to tnsparc.org

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dev.tnsparc.org",
+    baseUrl: "tnsparc.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
